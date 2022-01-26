1e180ae576425e895789bc316b856e34670f253d
AndreyBunesh <senior_21@mail.ru>
2022-01-26 20:27:34 +0300

    lessons 2

---
+++ Lesson2_2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Lesson2_2
+{
+ public class Program
+    {
+        public static void Main(string [] arg) 
+        {
+           Console.WriteLine("Enter number of month");
+            int Monthnumber = Convert.ToInt32(Console.ReadLine());
+            switch (Monthnumber)
+            {
+                case 1:
+                    {
+                        System.Console.WriteLine("This is January");
+                        break;
+                    }
+                case 2:
+                    {
+                        System.Console.WriteLine("This is February");
+                        break;
+                    }
+                case 3:
+                    {
+                        System.Console.WriteLine("This is March");
+                        break;
+                    }
+                case 4:
+                    {
+                        System.Console.WriteLine("This is April");
+                        break;
+                    }
+                case 5:
+                    {
+                        System.Console.WriteLine("This is May");
+                        break;
+                    }
+                case 6:
+                    {
+                        System.Console.WriteLine("This is June");
+                        break;
+                    }
+                case 7:
+                    {
+                        System.Console.WriteLine("This is July");
+                        break;
+                    }
+                case 8:
+                    {
+                        System.Console.WriteLine("This is August");
+                        break;
+                    }
+                case 9:
+                    {
+                        System.Console.WriteLine("This is September");
+                        break;
+                    }
+                case 10:
+                    {
+                        System.Console.WriteLine("This is October");
+                        break;
+                    }
+                case 11:
+                    {
+                        System.Console.WriteLine("This is November");
+                        break;
+                    }
+                case 12:
+                    {
+                        System.Console.WriteLine("This is December");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ Lessons2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lessons2_3
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                System.Console.WriteLine("This number is even");
+            }
+            else
+            {
+                System.Console.WriteLine("odd number");
+            }
+        }
+    }
+}
