cdf6805a959e62e04a36c7ec8b5ebdc2a98a372f
AndreyBunesh <senior_21@mail.ru>
2022-02-02 21:14:52 +0300

    Lesson3_draft

---
+++ Lesson_3/Lesson_3_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phonebookArray = new string[5, 2]
+            {
+                // Name, phone number/ e-mail
+                { "Alex","+130458585848/alex@test.com"},
+                { "Alex","130458585848/alex@test.com"},
+                { "Alex","130458585848/alex@test.com"},
+                { "Alex","+130458585848/alex@test.com"},
+                { "Yan","130458585848/alex@test.com"}
+            };
+            var height = phonebookArray.GetLength(0);
+            var weight = phonebookArray.GetLength(1);
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine($"name: {phonebookArray[i,0]}, phonenumber: {phonebookArray[i, 1]}");
+            }
+        }
+    }
+}
