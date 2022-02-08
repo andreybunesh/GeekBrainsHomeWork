3820e7de93eb341d6b9aae368c714a698b5d08c3
AndreyBunesh <senior_21@mail.ru>
2022-02-08 19:18:37 +0300

    Lesson_4 is created

---
+++ Lesson_4/Lesson_4_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lesson_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////string firstName = Console.ReadLine();
+            ////string lastName = Console.ReadLine();
+            //string patronymic = Console.ReadLine();
+            //string firstName1 = "Andrey";
+            //string lastName1 = "Bunesh";
+            //string patronymic1 = "Igorevich";
+            //string patronymic = Console.ReadLine();
+            string firstName1 = "Andrey";
+            string lastName1 = "Bunesh";
+            string patronymic1 = "Igorevich";
+            string firstName2 = "Vlad";
+            string lastName2 = "Drozd";
+            string patronymic2 = "Valerevich";
+            string firstName3 = "Alex";
+            string lastName3 = "Kozlov";
+            string patronymic3 = "Vladimirovich";
+            string firstName4 = "Dmitry";
+            string lastName4 = "Voronich";
+            string patronymic4 = "Valerevich";
+
+            GetFullName(firstName1, lastName1, patronymic1);
+            GetFullName(firstName2, lastName2, patronymic2);
+            GetFullName(firstName3, lastName3, patronymic3);
+            GetFullName(firstName4, lastName4, patronymic4);
+
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            Console.WriteLine($"{lastName} {firstName} {patronymic}");
+            return "SUCCESS";
+        }
+    }
+}
---
+++ Lesson_4/Lesson_4_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+//Написать программу, принимающую на вход строку — набор чисел,
+//разделенных пробелом, и возвращающую число — сумму всех чисел в строке.
+//Ввести данные с клавиатуры и вывести результат на экран.
+
+namespace Lesson_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any numbers separate space");
+            string allNumbers = Console.ReadLine();
+            int sum = 0;
+            string newLine = string.Empty;
+
+            for (int i = 0; i < allNumbers.Length; i++)
+            {
+
+                char currentNumber = allNumbers[i];
+                if (i == allNumbers.Length - 1)
+                {
+                    newLine += currentNumber;
+                    sum += Convert.ToInt32(newLine.ToString());
+                }
+                else if (currentNumber != ' ')
+                {
+                    newLine += currentNumber;
+                }
+                else
+                {
+                    sum += Convert.ToInt32(newLine.ToString());
+                    newLine = string.Empty;
+                }
+                                      
+            }
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
