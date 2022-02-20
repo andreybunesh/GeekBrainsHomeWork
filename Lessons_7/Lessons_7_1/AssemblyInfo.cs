47fd015a45921bfb3db9be88b3899796537d9bb1
AndreyBunesh <senior_21@mail.ru>
2022-02-20 23:13:48 +0300

    Lessons_7

---
+++ Lessons_7/Lessons_7_1/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Lessons_7_1")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Lessons_7_1")]
+[assembly: AssemblyTitle("Lessons_7_1")]
+[assembly: AssemblyVersion("1.0.0.0")]
