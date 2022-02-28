c912d3a6377343aee110663bf06db1d9617e7f05
AndreyBunesh <senior_21@mail.ru>
2022-02-28 14:31:39 +0300

    Lesson_8

---
+++ Lessons_8/Lessons_8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = Properties.Settings.Default.Greeting;
+            Console.WriteLine($"{greeting}");
+            string userName = Properties.Settings.Default.UserName;
+            string age = Properties.Settings.Default.Age;
+            string career = Properties.Settings.Default.Career;
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.UserName) || !string.IsNullOrEmpty(Properties.Settings.Default.Age) || !string.IsNullOrEmpty(Properties.Settings.Default.Career))
+            {
+                Console.WriteLine($" Имя пользователя: {userName}, Возраст: {age}, Профессия: {career}");
+            }
+            Console.WriteLine("Введите имя пользователя:");
+            Properties.Settings.Default.UserName = Console.ReadLine();
+            Console.WriteLine("Возраст:");
+            Properties.Settings.Default.Age = Console.ReadLine();
+            Console.WriteLine("Профессию");
+            Properties.Settings.Default.Career = Console.ReadLine();
+            Properties.Settings.Default.Save();
+            
+
+        }
+    }
+}
